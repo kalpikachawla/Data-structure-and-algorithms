8c9cbeb1db8b8f1c4727939f1be4302fdefa5ed4
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-01 16:30:47 +0530

    calculated the area of the sqaure

---
+++ areaofasquare.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the side of the sqaure : ";
+    cin>>a;
+    int area = a*a;
+    cout<<"The area of a sqaure is : "<<area;
+}
