0e51609162dcd61d3218fee660597333dd6472c9
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-01 17:24:59 +0530

    printed factorial of n

---
+++ factorial.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    int fact=1;
+    for(int i=2; i<=n; i++){
+        fact*=i;
+    }
+    cout<<"The factorial is : "<<fact;
+}
+    
