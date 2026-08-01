fcf8b4e4a8b7ebc7a3d0301ee7c28f14ec0d95c1
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-01 17:21:05 +0530

    printed sum of odd numbers upto n

---
+++ oddsum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n; 
+    cout<<"Enter n :";
+    cin>>n;
+    int oddsum = 0;
+    
+    for(int i=0; i<=n; i++){
+        if(i%2!=0){
+            oddsum+=i;
+        }
+    }
+    cout<<"Odd sum = "<<oddsum;
+}
