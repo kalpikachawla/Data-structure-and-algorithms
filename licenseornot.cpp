293c7de159338966eb1ca3ecf8933574687b195d
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-02 09:18:49 +0530

    application of if else

---
+++ licenseornot.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter age :";
+    cin>>n;
+    if(n>=18){
+        cout<<"The person is eligible to get a license";
+    }
+    else{
+        cout<<"The person is not eligible to get a license";
+    }// if else
+}
