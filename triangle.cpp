23ce700f4421337ba1716f8b8b16a30f0eee99c3
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-05 11:16:34 +0530

    printed triangle with spaces

---
+++ triangle.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void print(int n){
+    int spaces = 2*n-1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<j;
+        }
+        for(int j=1; j<=spaces; j++){
+            cout<<" ";
+        }
+        for(int j=i; j>=1; j--){
+            cout<<j;
+        }
+        cout<<endl;
+        spaces-=2;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    print(n);
+}
