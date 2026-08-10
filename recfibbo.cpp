4fbf92730bc19bc7bc42dc5d14142da529d65128
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-10 10:21:41 +0530

    printed fibbonacci series using recursion

---
+++ recfibbo.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int fibbo(int n){
+    if(n<=1) return n;
+    return fibbo(n-1) + fibbo(n-2);
+}
+int main(){
+    int n;
+    cout<<"Enter n :";
+    cin>>n;
+    for(int i=0; i<=n; i++){
+        cout<<fibbo(i)<<" ";
+    }
+}
