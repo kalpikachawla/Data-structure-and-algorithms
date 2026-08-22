a462542fbeab303bab814138fbe68108cf8cba67
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-22 13:08:48 +0530

    checked whether array is sorted and rotated or not

---
+++ check.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+bool check(int arr[], int n){
+    int count=0;
+    for(int i=1; i<n; i++){
+        if(arr[i-1]>arr[i]){
+            count++;
+        }
+    }
+    if(arr[n-1]>arr[0]){
+        count++;
+    }
+    return count<=1;
+}
+int main(){
+    int arr[5] = {1,3,4,5,8};
+    cout<<check(arr,5);
+}
