7d5a10151a4bc12547967f3fb230a8e02a090895
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-08-24 12:16:43 +0530

    maximum occuring character

---
+++ maxoccur.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+char maxocc(string s){
+    int arr[26] ={0};
+    for(int i=0; i<s.length(); i++){
+        char ch = s[i];
+        int number = 0;
+        number = ch - 'a';
+        arr[number]++;
+    }
+    int max = -1, ans = -1;
+    for(int i=0; i<26; i++){
+        if(max<arr[i]){
+            ans = i;
+            max = arr[i];
+        }
+    }
+    return 'a'+ans;
+}
+int main(){
+    string s;
+    cin>>s;
+    cout<<maxocc(s);
+}
