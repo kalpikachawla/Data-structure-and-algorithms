238a200705c7f486a562074fd919690baf70bc01
Kalpika Chawla <kalpikachawla17@gmail.com>
2026-09-02 09:16:14 +0530

    defining a macro

---
+++ macros.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#define PI 3.14
+int main(){
+    int r;
+    cout<<"Enter r :";
+    cin>>r;
+    double area = PI * r * r;
+    cout<<"Area is : "<<area;
+}
